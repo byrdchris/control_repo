f186bc89bc01f66e2a86a42dc71f74e0c523809f
byrdchris <60207346+byrdchris@users.noreply.github.com>
2021-09-09 13:46:32 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'pm01.evilkittens.com' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}\n",
     }
 }
 node /^web/ {
